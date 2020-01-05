d87031b206d4048adbece08b4a9801a2d5b2afdc
T.J <thomas.turnage@noaa.gov>
2020-01-05 13:43:37 -0500

    Add files via upload

--- mesowest_case_study.py
+++ mesowest_case_study.py
@@ -6,29 +6,22 @@ Created on Sun Jan 20 21:30:11 2019
 @author: tj
 
 """
+"""
+def station_plot(df):
+    stationplot = StationPlot(ax, df['lon'].values, df['lat'].values, clip_on=True,
+                          transform=ccrs.PlateCarree(), fontsize=10)
+
+    stationplot.plot_parameter('NW', df['temp'], color='red')
+    stationplot.plot_parameter('SW', df['dewpoint'], color='darkgreen')
+    sfc_df = pd.DataFrame(plot_list, columns = ['station','index_time','ob_time','lat','lon','temp',
+                                                'dewpoint','wind_dir','wind_speed','wgst'])
+
+    #sfc_df.dropna(inplace=True,subset=['temp','dewpoint','wind_dir', 'wind_speed'])
+    point_locs = proj.transform_points(ccrs.PlateCarree(), sfc_D['lon'].values, sfc_D['lat'].values)
+    u, v = wind_components((sfc_df['wind_speed'].values * units('m/s')).to('knots'),
+                       sfc_df['wind_dir'].values * units.degree)
 
 """
-def timeShift(timeStr,num,dt,direction):
-    times = []
-    steps = int(num)
-    minStart = int(steps * dt)
-    initTime = datetime.strptime(timeStr,'%Y%m%d%H%M')
-    if direction == 'backward':
-        origTime = initTime - timedelta(minutes=minStart)
-    else:
-        origTime = initTime   
-
-    for x in range(0,steps):
-        mins = x * dt
-        newTime = origTime + timedelta(minutes=mins)
-        nextTime = newTime + timedelta(minutes=dt)
-        newStr = datetime.strftime(newTime, '%Y%m%d%H%M')
-        new = datetime.strftime(newTime, '%Y-%m-%dT%H:%M:%S')
-        nextTimeStr = datetime.strftime(nextTime, '%Y-%m-%dT%H:%M:%S')
-        times.append([newStr,new,nextTimeStr])
-    return times
-"""
-
 import os
 import sys
 
@@ -41,21 +34,23 @@ except:
     windows = True
     sys.path.append('C:/data/scripts/resources')
     base_dir = 'C:/data'
+    image_dir = os.path.join('C:/data','images')
 
-import math
-import requests
+
 import numpy as np
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta,timezone 
-from metpy.calc import reduce_point_density
+from datetime import timezone 
 from metpy.calc import wind_components
+
 #from metpy.cbook import get_test_data
-from metpy.plots import add_metpy_logo, current_weather, sky_cover, StationPlot, wx_code_map
+from metpy.plots import StationPlot
+#from metpy.plots import add_metpy_logo, current_weather, sky_cover, wx_code_map
 from metpy.units import units
 from gis_layers import shape_mini
 
-from mesowest_functions import str_to_fl,mesowest_data_from_latest_observation
+
+from mesowest_functions import mesowest_data_from_latest_observation
 from mesowest_functions import mesowest_get_timeseries,mesowest_get_current_observations
 
 
@@ -65,22 +60,9 @@ rda = this_case['rda']
 extent = this_case['sat_extent']
 extent = [-88.4,-84.0,42.5,45.3]
 orig_extent = extent
-pd = this_case['pandas']
-p_ts = pd[0]
-p_steps = pd[1]
-p_int = pd[2]
+
 shapelist = this_case['shapelist']
 case_dir = os.path.join(base_dir,event_date)
-
-import pandas as pd
-df = None
-
-fout = 'C:/data/timeseries.txt'
-df = pd.read_csv(fout, index_col='newTimeStr', 
-                 names=['newTimeStr','time', 'station_id', 'lat', 'lon', 'temp', 'dewpoint','wdir','wspd','wgst'])
-
-formatT = "%Y-%m-%dT%H:%M:%SZ"
-
 
 shortDict = {'air_temp_value_1':'t',
              'dew_point_temperature_value_1d':'dp',
@@ -89,95 +71,159 @@ shortDict = {'air_temp_value_1':'t',
              'wind_gust_value_1':'wgst',
              'visibility_value_1':'vis'}
 
-
-stnDict2 = {'t':{'threshold':100,'color':'200 100 100','position':'-17,13, 1,'},
-          'dp':{'threshold':100,'color':'0 255 0','position':'-17,-13, 1,'},
-          'wspd':{'threshold':500,'color':'255 255 255','position':'NA'},
-          'wdir':{'threshold':500,'color':'255 255 255','position':'NA'},
-          'wgst':{'threshold':300,'color':'255 255 255','position':'NA'},
-          'vis':{'threshold':125,'color':'180 180 255','position':'17,-13, 1,'},
-          'rt':{'threshold':125,'color':'255 255 0','position':'17,13, 1,'}}
-
-
-#varStr = 'visibility,road_temp'
-varStr = 'air_temp,dew_point_temperature,wind_speed,wind_direction,wind_gust,visibility,road_temp'
-unitsStr = 'temp|F,speed|kts,precip|in'
-dstFile = 'C:/data/20190720-obs.txt'
+# Build list of variable names from shortDict
 varList =[]
 for keys in shortDict:
     varList.append(str(keys))
 
-#https://developers.synopticdata.com/about/station-variables/
-#https://synopticlabs.org/demos/lookup/?&lookup=networks
 
-API_ROOT = "https://api.synopticdata.com/v2/"
-API_TOKEN = "292d36a692d74badb6ca011f4413ae1b"
+# ---------------------------------------------------------
+#                Station list section
+# ---------------------------------------------------------
+create_new_stations_list = False
 
-nowTime = datetime.utcnow()
-nowTimeStr = datetime.strftime(nowTime,'%b %d,%Y %H:%M UTC')
-nowTimeStr2 = datetime.strftime(nowTime,'%Y%m%d%H%M')
+stations_list = ['KACB','KAMN','KBIV','KCAD','KETB','KGOV','KGRB','KGRR','KHTL',
+             'KLDM','KMBL','KMKG','KMOP','KMTW','KRQB','KSBM','KSUE','KTVC',
+             'BDWM4','SBDM4','BDLM4','GYGM4','LEOM4','EVAM4','WLLM4','KFKS',
+             '45007','MKGM4','MLWW3','SGNW3','KFFX','KY70','NMIM4','KOCQ','RSCM4','K3D2']
+
+if create_new_stations_list: 
+    jas2,stations_list = mesowest_get_current_observations()
+
+stns = ','.join(stations_list)
 
 
-obsDict = {}
-
-fout = 'C:/data/timeseries.txt'
-jas2,stns_list,line = mesowest_get_current_observations()
-stns = ','.join(stns_list)
-jas_ts = mesowest_get_timeseries('201907200000','201907201930',stns,fout)
+# ---------------------------------------------------------
+#                Time series data section
+# ---------------------------------------------------------
+request_new_timeseries_data = False
 
 import pandas as pd
-sfc_D = None
-sfc_D = pd.read_csv(fout, header=0, index_col=['time'],parse_dates=True,names=['time', 'station_id', 'lat', 'lon', 'temp', 'dewpoint','wdir','wspd','wgst'])
 
+# retrieve pandas time series information for the case
+p = this_case['pandas']
+p_ts = p[0]                 # initial time
+p_steps = p[1]              # number of steps
+p_int = p[2]                # time interval
+
+# initialize pandas date range
 idx = None
 idx = pd.date_range(p_ts, periods=p_steps, freq=p_int)
-dt = idx[1] - idx[0]
-#dt_sfc = dt * 6
-orig = idx[0]
-py_dt = orig.to_pydatetime()
+
+# create time strings of start and end times to pass as
+# agruments for requesting time series data from mesowest
+start_time_pd = idx[0]
+end_time_pd = idx[-1]
+start_time = start_time_pd.strftime('%Y%m%d%H%M')
+end_time = end_time_pd.strftime('%Y%m%d%H%M')
+
+py_dt = start_time_pd.to_pydatetime()
 orig_time = py_dt.replace(tzinfo=timezone.utc).timestamp()
 
-proj = ccrs.LambertConformal(central_longitude=-87, central_latitude=43,
-                             standard_parallels=[35])
+ts_file = 'C:/data/timeseries.txt'
+
+if request_new_timeseries_data:
+    mesowest_get_timeseries(start_time,end_time,stations_list,ts_file)
+
+# create pandas dataframe for time series data
+sfc_D = None
+sfc_D = pd.read_csv(ts_file, header=0, index_col=['time'],parse_dates=True,
+                    names=['time', 'station_id', 'lat', 'lon', 'temp',
+                           'dewpoint','wdir','wspd','wgst']) 
+
+   
+# ---------------------------------------------------------
+#                Radar data section
+# ---------------------------------------------------------
+
+# Create list of radar files, their filepaths, and their valid times
+from my_functions import make_radar_array, add_radar_paths, figure_timestamp
+
+
+met_info = []
+radar_dir = os.path.join(case_dir,rda,'netcdf/ReflectivityQC/00.50')
+add_radar_paths(radar_dir,'.','r',met_info)
+
+# make a dataframe of radar file paths using valid times as index
+np_met_info = np.array(met_info)
+metdat_D = pd.DataFrame(data=np_met_info[1:,1:],index=np_met_info[1:,0])  # 1st row as the column names
+metdat_D.columns = ['data_type', 'file_path']
+
+# ---------------------------------------------------------
+#                Radar data section
+# ---------------------------------------------------------
+from custom_cmaps import plts
+from my_functions import latest_file
+
 
 
 for i in range(0,len(idx)):
-    plot_list = []
-    sfc_df = []
-    for station in stns_list:
-        stn_data = mesowest_data_from_latest_observation(sfc_D,idx[i],station)
-        if stn_data is not None:
-            plot_list.append(stn_data)
+    new_datetime = idx[i]
 
-    sfc_df = pd.DataFrame(plot_list, columns = ['station','index_time','ob_time','lat','lon','temp',
-                                                'dewpoint','wind_dir','wind_speed','wgst'])
-
-    #sfc_df.dropna(inplace=True,subset=['temp','dewpoint','wind_dir', 'wind_speed'])
-    point_locs = proj.transform_points(ccrs.PlateCarree(), sfc_D['lon'].values, sfc_D['lat'].values)
-    u, v = wind_components((sfc_df['wind_speed'].values * units('m/s')).to('knots'),
-                       sfc_df['wind_dir'].values * units.degree)
-
+    fig_title,fig_fname_tstr = figure_timestamp(py_dt)
 
     plt.rcParams['savefig.dpi'] = 255
 
 # Create the figure and an axes set to the projection.
-    fig = plt.figure(figsize=(20, 10))
-    add_metpy_logo(fig, 1080, 290, size='large')
-    ax = fig.add_subplot(1, 1, 1, projection=proj)
-    extent = [-88.4,-84.0,42.5,45.3]
-# Add some various map elements to the plot to make it recognizable.
+    #fig = plt.figure(figsize=(20, 10))
+    rows = 1
+    cols = 1
+    figure_size =(10,10)
+    proj = ccrs.PlateCarree()
+    fig, ax = plt.subplots(rows,cols,figsize=figure_size,subplot_kw={'projection': ccrs.PlateCarree()})
+
+    #add_metpy_logo(fig, 1080, 290, size='large')
+
+
+    extent = [-88.0,-84.2,43.0,45.3]
     ax.set_extent(extent, crs=ccrs.PlateCarree())
     for sh in shape_mini:
         ax.add_feature(shape_mini[sh], facecolor='none', edgecolor='gray', linewidth=0.5)
         ax.tick_params(axis='both', labelsize=8)
 
-    stationplot = StationPlot(ax, sfc_df['lon'].values, sfc_df['lat'].values, clip_on=True,
-                          transform=ccrs.PlateCarree(), fontsize=10)
+    radar_file = latest_file(metdat_D,new_datetime,'r')
+    ra_filled,rlats,rlons = make_radar_array(radar_file,'Ref')
+    cs = ax.pcolormesh(rlats,rlons,ra_filled,cmap=plts['Ref']['cmap'],vmin=plts['Ref']['vmn'], vmax=plts['Ref']['vmx'])
+    #ax.pcolormesh(rlats,rlons,ra_filled,cmap=plts['Ref']['cmap'],vmin=plts['Ref']['vmn'], vmax=plts['Ref']['vmx'])
 
-    stationplot.plot_parameter('NW', sfc_df['temp'], color='red')
-    stationplot.plot_parameter('SW', sfc_df['dewpoint'],
-                           color='darkgreen')
+    plot_surface_observations = True
+    if plot_surface_observations:
+        plot_list = []
+        for station in stations_list:
+            stn_data = mesowest_data_from_latest_observation(sfc_D,idx[i],station)
+            if stn_data is not None:
+                plot_list.append(stn_data)
+    
+        sfc_df = None
+        sfc_df = pd.DataFrame(plot_list, columns = ['station','index_time','ob_time','lat','lon','temp',
+                                                    'dewpoint','wind_dir','wind_speed','wgst'])
+    
+        point_locs = proj.transform_points(ccrs.PlateCarree(), sfc_D['lon'].values, sfc_D['lat'].values)
+        #u, v = wind_components((sfc_df['wind_speed'].values * units('m/s')).to('knots'),
+        #                   sfc_df['wind_dir'].values * units.degree)
+        u, v = wind_components((sfc_df['wind_speed'].values * units('knots')),sfc_df['wind_dir'].values * units.degree)
+    
+        stationplot = StationPlot(ax, sfc_df['lon'].values, sfc_df['lat'].values, clip_on=True,
+                              transform=ccrs.PlateCarree(), fontsize=10)
+    
+        stationplot.plot_parameter('NW', sfc_df['temp'], color='#EE0000')
+        stationplot.plot_parameter('SW', sfc_df['dewpoint'],
+                               color='#009900')
+    
+        stationplot.plot_barb(u, v)
+
+    cs = ax.pcolormesh(rlats,rlons,ra_filled,cmap=plts['Ref']['cmap'],vmin=plts['Ref']['vmn'], vmax=plts['Ref']['vmx'])
+    
 
     stationplot.plot_barb(u, v)
 
-    plt.show()        
+    fig_fname_tstr = 'sfc_obs_' + idx[i].strftime('%Y%m%d_%H%M')
+    image_dst_path = os.path.join(image_dir,fig_fname_tstr + '.png')
+    #plt.tight_layout()
+
+
+    plot_title = 'Surface Observations - ' + idx[i].strftime('%d %b %Y %H%M UTC')
+    ax.set_title(plot_title)
+    #plt.savefig(image_dst_path,format='png')
+    plt.show()
+    plt.close()
